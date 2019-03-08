a5b1498f12bda6b3f15c7f7c3f18a0190854b497
fmartinez <fmartinez@easysol.net>
2019-03-07 19:46:42 -0500

    se creo proyecto ejemplo https://grokonez.com/spring-framework/use-spring-jpa-postgresql-spring-boot

--- src/main/java/com/artemis/registerTest/RegisterTestApplication.java
+++ src/main/java/com/artemis/registerTest/RegisterTestApplication.java
@@ -1,13 +1,27 @@
 package com.artemis.registerTest;
 
+import com.artemis.registerTest.repo.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegisterTestApplication {
+public class RegisterTestApplication implements CommandLineRunner {
+
+	@Autowired
+	CustomerRepository repository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RegisterTestApplication.class, args);
+
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// clear all record if existed before do the tutorial with new data
+		repository.deleteAll();
+	}
 }
---
+++ src/main/java/com/artemis/registerTest/repo/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.artemis.registerTest.repo;
+
+import com.artemis.registerTest.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+}
